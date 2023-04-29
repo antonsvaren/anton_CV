8652a217ec35d66983aec270c560a5483bf214bd
antonsvaren <gussvaanc@student.gu.se>
2023-04-29 23:54:55 +0200

    ...

--- vite-project/src/contact.jsx
+++ vite-project/src/contact.jsx
@@ -1,10 +1,33 @@
 import { Button, Typography } from "@mui/material";
-import profilePicture from "../assets/profilePicture.jpeg";
+import profilePicture from "../assets/profilePicture.png";
+import { useEffect, useRef } from "react";
+
 export default function Contact() {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.src = profilePicture;
+    img.onload = () => {
+      const width = img.width;
+      const height = img.height;
+      const size = Math.min(width, height);
+      canvas.width = size;
+      canvas.height = size;
+      ctx.beginPath();
+      ctx.arc(size / 2, size / 2, size / 2, 0, 2 * Math.PI);
+      ctx.closePath();
+      ctx.clip();
+      ctx.drawImage(img, (width - size) / 2, (height - size) / 2, size, size, 0, 0, size, size);
+    };
+  }, []);
+
   return (
     <div style={{display : "flex"}}>
       <Typography>THIS IS MY CONTACT!</Typography>
-      <img src={profilePicture} style= {{scale : "0.6", position : "absolute", right : "0", paddingBottom : "-300px"}}/>;
+      <canvas ref={canvasRef} style={{ scale : "0.8", position: "absolute", right: "0", paddingBottom: "100px", paddingLeft: "100px", paddingRight: "100px"}} />
     </div>
   );
 }
